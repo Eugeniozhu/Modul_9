d4e824ca667fc375b3803e0f1805474a7c4c7a24
Eugeniozhu <137764212+Eugeniozhu@users.noreply.github.com>
2024-12-27 19:02:07 +0500

    Add files via upload

---
+++ 9_2.py
@@ -0,0 +1,25 @@
+import math
+
+def is_prime(func):
+
+    def wrapper(*args):
+        func(*args)
+        i = sum(args)
+        factorial_ = math.factorial(i - 1)
+        if (factorial_ + 1) % i == 0:
+            print("Простое число")
+            return i
+        else:
+            print("Не простое число")
+            return i
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    # print(f'Выполняемая функция с агументами: {a}, {b}, {c}!')
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)
+print(result)
